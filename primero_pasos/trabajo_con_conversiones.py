2eaf7bdec10048e24e0754e37d6a11db17e429df
JoseMMV <josemanuelmorenovalenzuela@gmail.com>
2026-02-12 18:27:08 +0100

    conversiones

---
+++ primero_pasos/trabajo_con_conversiones.py
@@ -0,0 +1,27 @@
+a = "15"
+
+b = "35"
+
+print(a + b)
+print(int(a) + int(b))
+
+c = "15.25"
+
+d = "35.25"
+
+print(c + d)
+print(float(c) + float(d))
+
+edad = 28
+
+print("Mi edad es " + str(edad))
+
+empleados = ["Ana", "Óscar", "María", "Pedro", "Juan"]
+
+print(" ".join(empleados))
+print(empleados)
+
+empleados1 ="Ana, Óscar, María, Pedro, Juan"
+print(empleados1)
+
+print(empleados1.split())
